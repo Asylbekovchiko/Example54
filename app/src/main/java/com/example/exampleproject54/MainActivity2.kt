514f08d115af46e4d19a31823453ac3c6d9114ec
asylbekov.chiko@gmail.com <asylbekov.chiko@gmail.com>
2023-08-25 18:53:07 +0600

    some conflicts

--- app/src/main/java/com/example/exampleproject54/MainActivity2.kt
+++ app/src/main/java/com/example/exampleproject54/MainActivity2.kt
@@ -4,8 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity2 : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        setContentView(R.layout.activity_main2)
+//    }
 }
